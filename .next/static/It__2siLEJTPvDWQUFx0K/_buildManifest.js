908887bf04a435b71029e824ec29e0ae8ca88dd1
zhengfen <1582893052@qq.com>
2020-03-31 00:06:14 +0800

    gogogo

---
+++ .next/static/It__2siLEJTPvDWQUFx0K/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/edit":[a],"/home":[a],"/index":[a],"/person":[a],"/post":[a],"/sendpost":[a]}}("static\u002Fchunks\u002Fde27333be1827c98947c718bfd54b7d9278e0065.df3ef21fe7530626aade.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
